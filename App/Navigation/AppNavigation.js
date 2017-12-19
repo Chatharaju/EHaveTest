3865ed3778dd7af135f5e21f60f11ddbbdf29d3c
Vinay chatharaju <vinaychatharaju@Vinays-MacBook-Pro.local>
2017-12-19 12:38:47 -0500

    Add screens to Navigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,19 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import PaitentScreen from '../Containers/PatientScreen'
+import PaitentResultScreen from '../Containers/PatientResultsScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+    PaitentScreen: { screen: PaitentScreen },
+    PaitentResultScreen: { screen: PaitentResultScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'PaitentScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
