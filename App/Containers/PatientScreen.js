1fbaf1ce58bc894009fca027974e79b828dafc6a
Vinay chatharaju <vinaychatharaju@Vinays-MacBook-Pro.local>
2017-12-19 12:42:14 -0500

    Add background Image to Paitent Screen

--- App/Containers/PatientScreen.js
+++ App/Containers/PatientScreen.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 
+import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/PatientScreenStyles'
 
 export default class PatientScreen extends Component {
+
+  constructor() {
+    super()
+  }
+
   render () {
     return (
       <View style={styles.mainContainer}>
+        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
           <View style={styles.section} >
             <Text style={styles.sectionText}>
